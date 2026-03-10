564f7637e48c6b97fa65ee27bfc9bcb42b8ac4f0
ArjavTripathi <arjavtri@iastate.edu>
2026-03-10 17:50:00 -0500

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-medium underline">
+        Hello World!
+      </p>
+    </>
+  )
+}
+
+export default App
