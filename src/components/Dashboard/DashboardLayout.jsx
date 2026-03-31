71a54ceddfdbe5a879302cd984d79b1d21fbf904
ArjavTripathi <arjavtri@iastate.edu>
2026-03-31 16:50:43 -0500

    Bug where data is not fetched.

--- src/components/Dashboard/DashboardLayout.jsx
+++ src/components/Dashboard/DashboardLayout.jsx
@@ -5,9 +5,11 @@ import { useStoreContext } from '../../ContextApi/contextapi'
 import { useFetchTotalClicks } from '../../hooks/useQuery'
 
 const DashboardLayout = () => {
-  const { token } = useStoreContext;
+  const { token } = useStoreContext();
+  console.log("token:", token);
 
-  console.log(useFetchTotalClicks(token, onError));
+  const {isLoading: loader, data: totalClicks = []} = useFetchTotalClicks(token, onError);
+  console.log("loader:", loader, "totalClicks:", totalClicks);
 
   function onError(){
     console.log("error");
@@ -15,9 +17,23 @@ const DashboardLayout = () => {
  
   return (
     <div className='lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]'> 
+      { loader ? ( 
+        <p> Loading... </p>
+      ) : ( 
         <div className='lg:w-[90%] w-full mx-auto py-16'>
           <div className='h-96 relative'>
-            <Graph graphData={dummyData}/>
+            {totalClicks.length === 0 && (
+                     <div className="absolute flex flex-col  justify-center sm:items-center items-end  w-full left-0 top-0 bottom-0 right-0 m-auto">
+                     <h1 className=" text-slate-800 font-serif sm:text-2xl text-[18px] font-bold mb-1">
+                       No Data For This Time Period
+                     </h1>
+                     <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-sm text-slate-600 ">
+                       Share your short link to view where your engagements are
+                       coming from
+                     </h3>
+                   </div>
+                )}
+            <Graph graphData={totalClicks}/>
           </div>
           <div className='py-5 sm:text-end text-center'>
             <button className='bg-custom-gradient px-4 py-2 rounded-md'> 
@@ -25,6 +41,7 @@ const DashboardLayout = () => {
             </button>
           </div>
         </div>
+        )}
     </div>
   )
 }
